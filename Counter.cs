9fab1f76667cdbad3ade3600e3fdabac34340fa7
Sevgi <64860005+GIGI-bit@users.noreply.github.com>
2023-10-03 00:19:50 +0400

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,25 @@
+﻿namespace Directories
+{
+    internal static class Counter
+    {
+        public static int Count(string filePath)
+        {
+            using FileStream folder = new FileStream(filePath, FileMode.Open,FileAccess.Read);
+            StreamReader readStream = new StreamReader(folder);
+            string count = readStream.ReadLine();
+            if (count == null) return 0;
+            else if (count == "") return 0;
+            else return Int32.Parse(count);
+        }
+
+        public static void IncreaseCount(string filePath)
+        {
+            int temp = Count(filePath);
+            using FileStream folder = new FileStream(filePath, FileMode.Open, FileAccess.Write);
+            using var streamWriter = new StreamWriter(folder);
+            string count = Convert.ToString(temp + 1);
+            streamWriter.WriteLine(count);
+            folder.Flush ();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace NewDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap image;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
